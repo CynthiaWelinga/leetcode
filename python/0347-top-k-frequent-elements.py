1247f00e67053bfb0624f57f94e025ec8932c004
Cynthia Welinga <42123588+CynthiaWelinga@users.noreply.github.com>
2023-10-08 17:23:27 -0400

    Update 0347-top-k-frequent-elements.py
    if len(i)>0: # if that index(count) list has some values, add that to the result

--- python/0347-top-k-frequent-elements.py
+++ python/0347-top-k-frequent-elements.py
@@ -16,3 +16,26 @@ class Solution:
                     return res
 
         # O(n)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # We can use a dictionary to keep track of the frequency of the elements
+        # The key will be the number itself and the value will be the count of the number    
+        num_dict =  defaultdict(int) 
+
+        # # the most number of times a value could occur is len(nums). It's bounded by the size of the input array
+        freq = [[] for i in range(len(nums)+1)] # have a mapping the counts for each value
+
+        for i in nums:
+            num_dict[i]+=1 # increment the count of each value after each occurrence 
+
+        for key,value in num_dict.items(): # go through counted values and insert the value into the frequency index
+            freq[value].append(key) # append a list to the index position which represent the counts, all the elements of that index's occurence 
+        
+        res = []
+        for i in freq[::-1]: # go through bucket list of sorted frequencies from the end to get top k
+            if len(i)>0: # if that index(count) list has some values, add that to the result
+                res.extend(i) 
+                print(res)
+                if len(res)==k: # stop when you have the k top elements
+                    break
+        return res 
